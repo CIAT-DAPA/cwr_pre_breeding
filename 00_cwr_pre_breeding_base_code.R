98c5aa5f8421810bf6dfbbff5146edae7a16f91c
haachicanoy <harold22010@gmail.com>
2017-04-17 10:01:14 -0500

    Explaining code structure

--- 00_cwr_pre_breeding_base_code.R
+++ 00_cwr_pre_breeding_base_code.R
@@ -6,18 +6,21 @@
 
 options(warn = -1); options(scipen = 999); g <- gc(reset = T); rm(list = ls())
 
-library(raster)
+suppressMessages(if(!require(raster)){install.packages('raster'); library(raster)} else {library(raster)})
 
 # loop by crop
 
 # Step 1: Crop area identification
-source("")
+source("//dapadfs/Workspace_cluster_9/CWR_pre-breeding/Scripts/Datos_presencia_potato.R")
+crop_area_id(crop = "potato")
 
 # Step 2: Crop cycle identification by pixel
-source("")
+source("//dapadfs/Workspace_cluster_9/CWR_pre-breeding/Scripts/Ciclo_cultivo_potato.R")
+crop_cycle_id(crop = "potato", sys_type = "rainfed") # rainfed, irrigated
 
 # Step 3: Extract current climate data
-source("")
+source("//dapadfs/Workspace_cluster_9/CWR_pre-breeding/Scripts/extractCurrentClimate.R")
+current_climate(crop = "potato")
 
 # Step 4: Index calculation for current data
 source("")
