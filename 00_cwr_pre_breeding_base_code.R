a09dec2f9ad18c2243ed5f5fae98b23307929e34
haachicanoy <harold22010@gmail.com>
2017-04-17 09:34:06 -0500

    Base code for runs

---
+++ 00_cwr_pre_breeding_base_code.R
@@ -0,0 +1,30 @@
+# CWR pre-breeding characterising testing environments: base code
+# Authors: B. Mora & H. Achicanoy
+# CIAT, 2017
+
+## Bias-correction & downscaling for future data
+
+options(warn = -1); options(scipen = 999); g <- gc(reset = T); rm(list = ls())
+
+# loop by crop
+
+# Step 1: Crop area identification
+source("")
+
+# Step 2: Crop cycle identification by pixel
+source("")
+
+# Step 3: Extract current climate data
+source("")
+
+# Step 4: Index calculation for current data
+source("")
+
+# Step 5: Extract future climate
+source("")
+
+# Step 6: Index calculation for future data
+source("")
+
+# Step 7: Calculate similarity index according to pre-breeding sites
+source("")
