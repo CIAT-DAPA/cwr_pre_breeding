03c0a99b2f1188082211a91f16a58cd3035fea61
haachicanoy <harold22010@gmail.com>
2018-03-02 13:16:50 -0500

    Updates

--- 00_extract_future_climate.R
+++ 00_extract_future_climate.R
@@ -1,15 +1,48 @@
-library(raster)
-library(sp)
-library(rgdal)
-library(velox)
-library(tidyverse)
+# CWR pre-breeding characterising testing environments: extract future climate
+# Authors: B. Mora & H. Achicanoy
+# CIAT, 2018
 
-avlb_rst <- list.files(path = "//ccafsserver.cgiarad.org/data_climatewizard/AR5_Global_Daily_25k", pattern = "*.nc$", full.names = F)
-avlb_rst <- avlb_rst[setdiff(1:length(avlb_rst), grep(pattern = "historical", x = avlb_rst))]
-avlb_rst <- avlb_rst[-1]
+# Load R packages
+suppressMessages(if(!require(raster)){install.packages('raster'); library(raster)} else {library(raster)})
+suppressMessages(if(!require(sp)){install.packages('sp'); library(sp)} else {library(sp)})
+suppressMessages(if(!require(rgdal)){install.packages('rgdal'); library(rgdal)} else {library(rgdal)})
+suppressMessages(if(!require(velox)){install.packages('velox'); library(velox)} else {library(velox)})
+suppressMessages(if(!require(tidyverse)){install.packages('tidyverse'); library(tidyverse)} else {library(tidyverse)})
 
-test <- data.frame(do.call('rbind', strsplit(as.character(avlb_rst),'_',fixed=TRUE)))
-colnames(test) <- c("Variable", "Time", "D1", "RCP", "D2", "GCM", "Year")
+# # Matching combinations GCM-RCP-time
+# c("BNU-ESM" = "bnu_esm")
+# c("CanESM2" = "cccma_canesm2")
+# c("CSIRO-Mk3-6-0" = "csiro_mk3_6_0")
+# c("GFDL-CM3" = "gfdl_cm3")
+# c("inmcm4" = "inm_cm4")
+# c("IPSL-CM5A-LR" = "ipsl_cm5a_lr")
+# c("IPSL-CM5A-MR" = "ipsl_cm5a_mr")
+# c("MIROC-ESM-CHEM" = "miroc_esm_chem")
+# c("MPI-ESM-LR" = "mpi_esm_lr")
+# c("MPI-ESM-MR" = "mpi_esm_mr")
+# c("NorESM1-M" = "ncc_noresm1_m")
+
+climateWizardSrc <- "//ccafsserver.cgiarad.org/data_climatewizard/AR5_Global_Daily_25k" # It has: prec, tmax, tmin
+rstList <- list.files(path = climateWizardSrc, pattern = "*.nc$", full.names = F)
+rstList <- rstList[setdiff(1:length(rstList), grep(pattern = "historical", x = rstList))]
+rstList <- rstList[-1]
+grep2 <- Vectorize(FUN = grep, vectorize.args = "pattern")
+rstList <- rstList[grep2(pattern = paste(2020:2069), x = rstList)]
+rstList <- rstList[grep2(pattern = c("BNU-ESM", "CanESM2", "CSIRO-Mk3-6-0",
+                                     "GFDL-CM3", "inmcm4", "IPSL-CM5A-LR",
+                                     "IPSL-CM5A-MR", "MIROC-ESM-CHEM", "MPI-ESM-LR",
+                                     "MPI-ESM-MR", "NorESM1-M"), x = rstList)]
+df <- data.frame(do.call('rbind', strsplit(as.character(rstList), '_', fixed = TRUE)))
+colnames(df) <- c("Variable", "Time", "D1", "RCP", "D2", "GCM", "Year")
+gcmList <- df$GCM %>% as.character %>% unique
+
+ownSrc <- "//dapadfs/workspace_cluster_12/Trust"
+own_rcp45_gcmList <- list.dirs(path = paste0(ownSrc, "/gcm_rcp45/bc_0_5deg_lat"), full.names = F, recursive = F)
+own_rcp85_gcmList <- list.dirs(path = paste0(ownSrc, "/gcm_rcp85/bc_0_5deg_lat"), full.names = F, recursive = F)
+
+
+
+
 
 avlb_rst[setdiff(1:length(avlb_rst), grep(pattern = "^pr_day_", x = avlb_rst))]
 
